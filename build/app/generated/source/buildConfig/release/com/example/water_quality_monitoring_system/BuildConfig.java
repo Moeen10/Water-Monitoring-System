1c18162c79dc2b599f83c0700408769cccffc7a9
T.M Moeen Uddin <moeen.uddin@datasoft-bd.com>
2023-09-12 14:04:00 +0600

    APP DONE Motamuti

---
+++ build/app/generated/source/buildConfig/release/com/example/water_quality_monitoring_system/BuildConfig.java
@@ -0,0 +1,12 @@
+/**
+ * Automatically generated file. DO NOT MODIFY
+ */
+package com.example.water_quality_monitoring_system;
+
+public final class BuildConfig {
+  public static final boolean DEBUG = false;
+  public static final String APPLICATION_ID = "com.example.water_quality_monitoring_system";
+  public static final String BUILD_TYPE = "release";
+  public static final int VERSION_CODE = 1;
+  public static final String VERSION_NAME = "2.0.0";
+}
